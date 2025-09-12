8b78ece9335b1e127c2e2836b69ecc20d2433321
SanketTheCodeCrafter <sanket.nagap3@gmail.com>
2025-09-12 20:15:45 +0530

    refactor: streamline server port handling by using a single PORT constant

--- server/index.js
+++ server/index.js
@@ -16,6 +16,8 @@ import shopReviewRouter from './routes/shop/review-routes.js';
 
 dotenv.config();
 const app = express();
+const PORT = Number(process.env.PORT) || 5000;
+
 
 mongoose.connect(process.env.MONGO_URL)
 .then(()=>{
@@ -57,8 +59,8 @@ app.use('/api/shop/order', shopOrderRouter);
 app.use('/api/shop/search', shopSearchRouter);
 app.use('/api/shop/review', shopReviewRouter);
 
-app.listen(()=>{
-    console.log(`Server running on port ${process.env.PORT}`);
-})
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
 
 
